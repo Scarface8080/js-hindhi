3e332c2b41cd10c8f2c7e251ec4a5a276c1791cc
Hamza Shoukat <100609471+Scarface8080@users.noreply.github.com>
2024-10-11 19:28:09 +0000

    dataType_Summary

---
+++ 01_basic/dataTypes_summary.js
@@ -0,0 +1,38 @@
+// # Primitive datatype
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')   // even if we give same value still it will be consider different/unique using symbol sataType
+  
+console.log(id === anotherId);
+
+const bigNumber = 331233313346684621n
+
+
+
+
+
+// Reference Type (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Superman", "Batmen", "DeadPool"]
+
+let myObj = {
+    name:"Hamza",
+    age:24,
+}
+
+const myFunction = function (){
+    console.log("hello World");
+    
+}
+
+console.log(typeof anotherId);
